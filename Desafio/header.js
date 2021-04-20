f7398e51220a6cb7b60d585d14c6617e25b46cf5
João Vitor Cenedeze <73188212+poiozex@users.noreply.github.com>
2021-04-20 11:02:30 -0300

    Delete header.js

--- Desafio/header.js
+++
@@ -1,9 +0,0 @@
-document.write(`<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Geex Modern Dashboard</title>
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
-</head>`);
